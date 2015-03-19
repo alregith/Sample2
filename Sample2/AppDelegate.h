a0cfeabe2eadc18a7392b1c117a2826f904ed73c
Regith Lingesh <alregith@Regiths-MacBook-Pro.local>
2015-03-18 22:15:38 -0700

    Initial Commit

---
+++ Sample2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample2
+//
+//  Created by Regith Lingesh on 3/18/15.
+//  Copyright (c) 2015 Regith Lingesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample2
+//
+//  Created by Regith Lingesh on 3/18/15.
+//  Copyright (c) 2015 Regith Lingesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
